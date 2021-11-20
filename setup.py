3e49c90a083f7dbc99d4cf0c40e18b7c8bb1d51b
kadir pekel <kadirpekel@gmail.com>
2021-11-20 12:06:09 +0300

    fixed setup.py

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ setup(
     install_requires=[
         'py-algorand-sdk>=1.8.0',
         'pyteal==0.9.0',
-        'komandr>=1.0.2',
+        'komandr>=1.0.4',
     ],
     tests_require=[
         'pytest>=1.8.2'
     ],
     entry_points={
         'console_scripts': [
-            'algorig=algorig.cli:main',
+            'rig=algorig.cli:main',
         ],
     },
 )
