7e6e249357565557786c7ae86e54545f270fe89a
kadir pekel <kadirpekel@gmail.com>
2021-11-22 23:46:26 +0300

    fixed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='algorig',
     py_modules=['algorig'],
-    version='0.0.2',
+    version='0.0.3',
     python_requires='>3.5.4',
     description='algorand development rig',
     author='Kadir Pekel',
